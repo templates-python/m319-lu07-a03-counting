ba6891a1b9f4aa57fa6f8f605e5b931f2936e6de
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-11-02 14:26:11 +0100

    Testcases

--- main_test.py
+++ main_test.py
@@ -1,8 +1,8 @@
 from main import main
 
-def test_counting_for_loop(monkeypatch, capsys):
-    """Testet das Zählen mit einer Schleife durch Überprüfung des Outputs."""
-    monkeypatch.setattr('builtins.input', lambda: '4')
+def test_main_1(monkeypatch, capsys):
+    """Testet die Hauptfunktion durch Überprüfung des Outputs."""
+    monkeypatch.setattr('builtins.input', lambda _: '4')
     main()
     captured = capsys.readouterr()
-    assert captured.out == '0\n1\n2\n3\n4\n'
+    assert captured.out == '0\n1\n2\n3\n'
